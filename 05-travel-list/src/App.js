9ba7f41d335d2eb0062e0bab253e41bd59ac0c02
João Victor Calvet <joaovictor.calvet@gmail.com>
2026-02-05 20:33:28 -0300

    feature(travel): added reactive footer and item deletion

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -7,12 +7,28 @@ export default function App() {
     setItems((items) => [...items, item]);
   }
 
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item,
+      ),
+    );
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItem={handleAddItem} />
-      <PackingList items={items} />
-      <Stats />
+      <PackingList
+        items={items}
+        onToggleItem={handleToggleItem}
+        onDeleteItem={handleDeleteItem}
+      />
+      <Stats items={items} />
     </div>
   );
 }
@@ -61,33 +77,59 @@ function Form({ onAddItem }) {
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+            key={item.id}
+          />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+
+  const totalItems = items.length;
+  const totalPacked = items.filter((item) => item.packed).length;
+  const packedPercentage = Math.round((totalPacked / totalItems) * 100);
+
   return (
     <footer className="stats">
-      <em>💼 You have x items on your list, and you already packed X (x%)</em>
+      <em>
+        {packedPercentage === 100
+          ? "You got everything! Ready to go ✈"
+          : `💼 You have ${totalItems} items on your list, and you already packed
+          ${totalPacked} (${packedPercentage}%)`}
+      </em>
     </footer>
   );
 }
